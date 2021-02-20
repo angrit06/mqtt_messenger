0b136ab4297a09bf36aeead446066266db134fa6
GRA001 <andreas.greiss@heller.biz>
2021-02-20 20:15:48 +0100

    publish should run

---
+++ app/src/main/java/com/angrit06/mqtt_message_board/MainActivity.java
@@ -0,0 +1,87 @@
+package com.angrit06.mqtt_message_board;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+    String topic;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Snackbar", Snackbar.LENGTH_LONG)
+//                        .setAction("snack", null).show();
+//            }
+//        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch (view.getId()) {
+            case R.id.urgent:
+                if (checked)
+                    topic = "/mqtt_message_board/urgent";
+                Toast.makeText(getBaseContext(), topic, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.task:
+                if (checked)
+                    topic = "/mqtt_message_board/task";
+                Toast.makeText(getBaseContext(), topic, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.information:
+                if (checked)
+                    topic = "/mqtt_message_board/information";
+                Toast.makeText(getBaseContext(), topic, Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+}
